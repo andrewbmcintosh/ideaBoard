09dbec33bcd7410976963ec33a5db3a24af57478
andrewmcintosh <abmcintosh7@gmail.com>
2019-01-22 20:08:57 -0500

    got routes up and running

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import styled from 'styled-components'
+import Homepage from './Components/Homepage'
+import LoginPage from './Components/LoginPage'
+import IdeaPage from './Components/IdeaPage'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Router>
+        <div>
+          <div>
+            <Link to='/login'>Login</Link>
+          </div>
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/user/:userId" component={IdeaPage} />
+          </Switch>
+        </div>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ client/src/Components/IdeaPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class IdeaPage extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello from Idea Page</p>
+      </div>
+    )
+  }
+}
